4baf25faa26b61ec6ffa7abb19c6c91f77bc7011
Olivier Benz <olivier.benz@b-data.ch>
2023-08-26 20:25:58 +0200

    Fetch and update scripts

--- base/.pre/fetch-scripts.sh
+++ base/.pre/fetch-scripts.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+# Copyright (c) 2023 b-data GmbH.
+# Distributed under the terms of the MIT License.
 
 set -e
 
-files="start.sh start-notebook.sh start-singleuser.sh"
+scripts="start-notebook.sh start-singleuser.sh"
 
-for i in $files ; do
-  curl -sL https://raw.githubusercontent.com/jupyter/docker-stacks/main/base-notebook/$i \
-    -o scripts/usr/local/bin/$i
+for i in $scripts ; do
+  curl -sSL https://raw.githubusercontent.com/jupyter/docker-stacks/main/images/base-notebook/"$i" \
+    -o scripts/usr/local/bin/"$i"
+done
+
+scripts="start.sh run-hooks.sh"
+
+for i in $scripts ; do
+  curl -sSL https://raw.githubusercontent.com/jupyter/docker-stacks/main/images/docker-stacks-foundation/"$i" \
+    -o scripts/usr/local/bin/"$i"
 done
 
 chmod +x scripts/usr/local/bin/*.sh
