0c7eaca97cc15cd8739744ce4aafbb0142145e98
Olivier Benz <olivier.benz@b-data.ch>
2025-01-31 07:26:58 +0100

    Fix script 12-r.sh
    - Set HOME="/home/root" when run with rootless triplet
    - https://github.com/b-data/jupyterlab-r-docker-stack/issues/5

--- base/scripts/usr/local/bin/before-notebook.d/12-r.sh
+++ base/scripts/usr/local/bin/before-notebook.d/12-r.sh
@@ -5,6 +5,9 @@
 set -e
 
 if [ "$(id -u)" == 0 ] ; then
+  if [ "${NB_USER}" = "root" ] && [ "${NB_UID}" = "$(id -u "${NB_USER}")" ] && [ "${NB_GID}" = "$(id -g "${NB_USER}")" ]; then
+    HOME="/home/root"
+  fi
   # Create user's R package library
   RLU=$(run_user_group Rscript -e "cat(Sys.getenv('R_LIBS_USER'))")
   run_user_group mkdir -p "$RLU"
